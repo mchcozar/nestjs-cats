7f5a5abe470f9035f6757d1544386f883aca8533
Mario Cesar Hernandez <mchernandez@muniantigua.gob.gt>
2025-07-03 15:09:53 -0600

    first commit

---
+++ src/cats/entities/cat.entity.ts
@@ -0,0 +1,24 @@
+import { Breed } from "src/breeds/entities/breed.entity"
+import { Column, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class Cat {
+
+  // @PrimaryGeneratedColumn
+  @Column({ primary: true, generated: true})
+  id: number
+
+  @Column()
+  name: string
+
+  @Column()
+  age: number
+
+  @DeleteDateColumn()
+  deletedAt: Date
+
+  @ManyToOne(() => Breed, (breed) => breed.id, {
+    eager: true // Para que traiga las razas al hacer findOne
+  })
+  breed: Breed
+}
